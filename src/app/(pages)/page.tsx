a463aa0e6526c24eb5850b86d6bbaf52e0a10ae5
adnanmahmud0 <adnan99mahmud@gmail.com>
2025-05-30 03:04:10 +0600

    in the (page) home page all compunent are added

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,10 +1,21 @@
-import HeroSection from '@/components/Home/HeroSection/HeroSection';
+
+import Companies from '@/components/Home/Companies';
+import Courses from '@/components/Home/Courses';
+import Hero from '@/components/Home/Hero';
+import Mentor from '@/components/Home/Mentor';
+import Newsletter from '@/components/Home/Newsletter';
+import Testimonial from '@/components/Home/Testimonials';
 import React from 'react';
 
 const Home = () => {
     return (
         <>
-            <HeroSection/>
+            <Hero />
+            <Companies />
+            <Courses />
+            <Mentor />
+            <Testimonial />
+            <Newsletter />
         </>
     );
 };
