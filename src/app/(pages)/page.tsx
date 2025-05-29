6fb8f8421fc423cef8444528122d67944fcf0ecb
adnanmahmud0 <adnan99mahmud@gmail.com>
2025-05-30 03:50:39 +0600

    herosection added

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,11 +1,10 @@
-
+import React from 'react';
 import Companies from '@/components/Home/Companies';
 import Courses from '@/components/Home/Courses';
 import Hero from '@/components/Home/Hero';
 import Mentor from '@/components/Home/Mentor';
 import Newsletter from '@/components/Home/Newsletter';
 import Testimonial from '@/components/Home/Testimonials';
-import React from 'react';
 
 const Home = () => {
     return (
