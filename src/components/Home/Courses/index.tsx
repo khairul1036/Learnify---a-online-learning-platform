7ffbc75ef489c63c210ba4408c178d411d1442b3
adnanmahmud0 <adnan99mahmud@gmail.com>
2025-05-30 20:07:01 +0600

    companis header text is updated

--- src/components/Home/Courses/index.tsx
+++ src/components/Home/Courses/index.tsx
@@ -62,7 +62,7 @@ const Courses = () => {
         <section id="courses">
             <div className='container mx-auto lg:max-w-screen-xl md:max-w-screen-md px-4 pb-20'>
                 <div className="sm:flex justify-between items-center mb-20">
-                    <h2 className="text-midnight_text text-4xl lg:text-5xl font-bold mb-5 sm:mb-0">Popular courses.</h2>
+                    <h2 className="text-4xl lg:text-5xl font-bold mb-5 sm:mb-0">Popular courses.</h2>
                     <Link href={'/'} className="text-lg font-bold hover:tracking-widest duration-500">Explore courses&nbsp;&gt;&nbsp;</Link>
                 </div>
                 <Slider {...settings}>
